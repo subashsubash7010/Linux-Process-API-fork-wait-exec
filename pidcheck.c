3c8429f2680907bed8e1a6940f4893f2e6a60587
subashsubash7010 <subashmurugan7418@gmail.com>
2025-11-12 19:44:35 +0530

    Update pidcheck.c

--- pidcheck.c
+++ pidcheck.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -15,21 +14,3 @@ printf("I am parent, my pid is %d\n",getpid());
 sleep(100); 
 exit(0);} 
 }
-=======
-#include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include<stdlib.h>
-int main()
-{ int pid; 
-pid=fork(); 
-if(pid == 0) 
-{ printf("Iam child my pid is %d\n",getpid()); 
-printf("My parent pid is:%d\n",getppid()); 
-exit(0); } 
-else{ 
-printf("I am parent, my pid is %d\n",getpid()); 
-sleep(100); 
-exit(0);} 
-}
->>>>>>> 108f3915e5f93a42e79be3e4c03f8b21106c4a36
